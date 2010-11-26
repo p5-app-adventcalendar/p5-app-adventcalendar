d858b145740ad71beaf44bcfc20076f8e22baffb
Kan Fushihara <kan@mfac.jp>
2010-11-26 12:04:41 +0900

    initial commit

---
+++ lib/App/AdventCalendar.pm
@@ -0,0 +1,32 @@
+package App::AdventCalendar;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::AdventCalendar -
+
+=head1 SYNOPSIS
+
+  use App::AdventCalendar;
+
+=head1 DESCRIPTION
+
+App::AdventCalendar is
+
+=head1 AUTHOR
+
+Kan Fushihara E<lt>kan@mfac.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
